1bd64c12bb891c3e71323814eac614cafaafa301
Bruno Antunes <bmvantunes@gmail.com>
2020-06-11 11:12:47 +0100

    removing the 'bruno' folder that was left from a test =D

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,7 +1,6 @@
 import fetch from 'isomorphic-unfetch';
 import Link from 'next/link';
 import { VehiclePerson } from '../../api/VehiclePerson';
-import MyComp from './bruno';
 
 export interface ListProps {
   ownersList?: VehiclePerson[];
@@ -16,7 +15,6 @@ export default function List({ownersList}: ListProps) {
             <a>
               Navigate to {e.ownerName}'s {e.vehicle}
             </a>
-            <MyComp/>
           </Link>
         </div>
       ))}
